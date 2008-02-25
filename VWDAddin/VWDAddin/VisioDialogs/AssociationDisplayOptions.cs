e02b248a22e3f74a8dd269dbd1c5ccd69c03d4e8
Nexus.kun <Nexus.kun@4308edd9-fa46-0410-ae85-53b26f51fcf5>
2008-02-25 21:09:11 +0000

    Добавлен диалог Display Options для ассоциаций

--- VWDAddin/VWDAddin/VisioDialogs/AssociationDisplayOptions.cs
+++ VWDAddin/VWDAddin/VisioDialogs/AssociationDisplayOptions.cs
@@ -1,3 +1,4 @@
+using Shape = Microsoft.Office.Interop.Visio.Shape;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -6,7 +7,7 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 
-namespace VWDAddin.VisioDialogs
+namespace VWDAddin
 {
     public partial class AssociationDisplayOptions : Form
     {
@@ -15,9 +16,93 @@ namespace VWDAddin.VisioDialogs
             InitializeComponent();
         }
 
+        private void SetCheckBoxFromShape(CheckBox checkBox, Shape shape, string cellName)
+        {
+            if (shape.get_Cells(cellName).Formula.Equals("TRUE"))
+                checkBox.Checked = false;
+            else
+                checkBox.Checked = true;
+        }
+
+        private void SetShapeFromCheckBox(CheckBox checkBox, Shape shape, string cellName)
+        {
+            if (checkBox.Checked == true)
+                shape.get_Cells("HideText").Formula = "FALSE";
+            else
+                shape.get_Cells("HideText").Formula = "TRUE";
+        }
+
+        public AssociationDisplayOptions(Shape shape)
+        {
+            try
+            {
+
+                InitializeComponent();
+                if (shape != null)
+                {
+                    m_shape = shape;
+                    SetCheckBoxFromShape(DisplayName, shape, "HideText");
+                    foreach (Shape childShape in shape.Shapes)
+                    {
+                        string type = VisioHelpers.GetShapeType(childShape);
+                        switch (type)
+                        {
+                            case "end1_name":
+                                SetCheckBoxFromShape(DisplayEnd1Name, childShape, "HideText");
+                                break;
+                            case "end2_name":
+                                SetCheckBoxFromShape(DisplayEnd2Name, childShape, "HideText");
+                                break;
+                            case "end1_mp":
+                                SetCheckBoxFromShape(DisplayEnd1MP, childShape, "HideText");
+                                break;
+                            case "end2_mp":
+                                SetCheckBoxFromShape(DisplayEnd2MP, childShape, "HideText");
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                int abc = 0;
+            }
+        }
+
         private void OKButton_Click(object sender, EventArgs e)
         {
-            
+            SetShapeFromCheckBox(DisplayName, m_shape, "HideText");
+            foreach (Shape childShape in m_shape.Shapes)
+            {
+                string type = VisioHelpers.GetShapeType(childShape);
+                switch (type)
+                {
+                    case "end1_name":
+                        SetShapeFromCheckBox(DisplayEnd1Name, childShape, "HideText");
+                        break;
+                    case "end2_name":
+                        SetShapeFromCheckBox(DisplayEnd2Name, childShape, "HideText");
+                        break;
+                    case "end1_mp":
+                        SetShapeFromCheckBox(DisplayEnd1MP, childShape, "HideText");
+                        break;
+                    case "end2_mp":
+                        SetShapeFromCheckBox(DisplayEnd2MP, childShape, "HideText");
+                        break;
+                    default:
+                        break;
+                }
+            }
+            this.Close();
+        }
+
+        private Shape m_shape;
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
--- VWDAddin/VWDAddin/VisioEventHandlers/VisioHelpers.cs
+++ VWDAddin/VWDAddin/VisioEventHandlers/VisioHelpers.cs
@@ -24,11 +24,21 @@ namespace VWDAddin
             }
             return null;
         }
+<<<<<<< .mine
+
+        public static string GetShapeType(Shape shape)
+        {
+            string cellFormula = shape.get_Cells("user.type").Formula;
+            return cellFormula.Substring(1, cellFormula.Length - 2);
+        }
+    
+=======
 
         public static String ToString(String value)
         {
             if (value == null) value = String.Empty;
             return "\"" + value.Replace("\"", "\"\"") + "\"";
         }
+>>>>>>> .r18
     }
 }
