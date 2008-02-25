914e13e46617a2b79ae82a18cc9369f4b56c39e4
Nexus.kun <Nexus.kun@4308edd9-fa46-0410-ae85-53b26f51fcf5>
2008-02-25 14:57:08 +0000

    Почему-то это не добавилось в прошлый раз

---
+++ trunk/VWDAddin/VWDAddin/WordInterop/WordHelpers.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace VWDAddin {
+  class WordHelpers {
+    public static string GetName(Microsoft.Office.Interop.Visio.Shape shape, string shapeName) {
+      if (shape.Name == shapeName)
+        return shape.Text;
+      foreach (Microsoft.Office.Interop.Visio.Shape childShape in shape.Shapes) {
+        if (childShape.Name == shapeName)
+          return childShape.Text;
+      }
+      return string.Empty;
+    }
+
+    public static XmlNode CreateCustomNode(WordDocument doc, string elementName, int id) {
+      XmlNode customNode = doc.CreateNode(XmlNodeType.Element, "w:customXml", Definitions.WORD_PROCESSING_ML);
+      XmlAttribute attr = doc.CreateAttribute("w:element", Definitions.WORD_PROCESSING_ML);
+      attr.Value = elementName;
+      customNode.Attributes.Append(attr);
+      attr = doc.CreateAttribute("w:object_id", Definitions.WORD_PROCESSING_ML);
+      attr.Value = id.ToString();
+      customNode.Attributes.Append(attr);
+      return customNode;
+    }
+
+    public static XmlNode GetCustomChild(XmlNode node, string customName) {
+      foreach (XmlNode child in node.ChildNodes) {
+        if (child.Attributes[0].Value == customName)
+          return child;
+      }
+      return null;
+    }
+
+    public static XmlNode CreateTextChildNode(WordDocument doc, string text) {
+      XmlElement tagParagraph = doc.CreateElement("w:p", Definitions.WORD_PROCESSING_ML);
+      XmlElement tagRun = doc.CreateElement("w:r", Definitions.WORD_PROCESSING_ML);
+      tagParagraph.AppendChild(tagRun);
+      XmlElement tagText = doc.CreateElement("w:t", Definitions.WORD_PROCESSING_ML);
+      tagRun.AppendChild(tagText);
+      XmlNode nodeText = doc.CreateNode(XmlNodeType.Text, "w:t", Definitions.WORD_PROCESSING_ML);
+      nodeText.Value = text;
+      tagText.AppendChild(nodeText);
+      return tagParagraph;
+    }
+
+    public static string[] ConvertListToArray(List<string> list) {
+      string[] result = new string[list.Count];
+      int curr = 0;
+      foreach (string str in list) {
+        result[curr++] = str;
+      }
+      return result;
+    }
+
+    public static List<string> ConvertArrayToList(string[] array) {
+      List<string> result = new List<string>();
+      foreach (string str in array) {
+        result.Add(str);
+      }
+      return result;
+    }
+
+  }
+}
