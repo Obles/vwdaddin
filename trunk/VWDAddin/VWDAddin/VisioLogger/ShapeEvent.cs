612f874273a83df0a0a4e82752ebea468ad6500b
Nexus.kun <Nexus.kun@4308edd9-fa46-0410-ae85-53b26f51fcf5>
2008-03-02 19:45:05 +0000

    Добавлен класс AssociationEvent. Исправлена ошибка

--- trunk/VWDAddin/VWDAddin/VisioLogger/ShapeEvent.cs
+++ trunk/VWDAddin/VWDAddin/VisioLogger/ShapeEvent.cs
@@ -14,42 +14,24 @@ namespace VWDAddin.VisioLogger
 
         #region Members
         private string m_guid;
-        private string m_mainName;
-        private string m_end1Name;
-        private string m_end1MP;        //Multiplicity
-        private string m_end2Name;
-        private string m_end2MP;
+        private string m_className;
+        private string m_attributes;
 
         public string GUID
         {
             get { return m_guid; }
             //set { m_guid = value; }
         }
-        public string mainName
+        public string className
         {
-            get { return m_mainName; }
-            //set { m_mainName = value; }
+            get { return m_className; }
+            //set { m_className = value; }
         }
-        public string end1Name
+        public string attributes
         {
-            get { return m_end1Name; }
-            //set { m_end1Name = value; }
-        }
-        public string end1MP
-        {
-            get { return m_end1MP; }
-            //set { m_end1MP = value; }
-        }
-        public string end2Name
-        {
-            get { return m_end2Name; }
-            //set { m_end2Name = value; }
-        }
-        public string end2MP
-        {
-            get { return m_end2MP; }
-            //set { m_end2MP = value; }
-        }
+            get { return m_attributes; }
+            //set { m_attributes = value; }
+        }        
         #endregion
     }
 }
