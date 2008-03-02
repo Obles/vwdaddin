e8e9ccc0326210153544cbd88a916dddf72c1001
Nexus.kun <Nexus.kun@4308edd9-fa46-0410-ae85-53b26f51fcf5>
2008-03-02 19:39:00 +0000

    Добавлен класс ShapeEvent, который ответственен за все события с shape'oм

---
+++ trunk/VWDAddin/VWDAddin/VisioLogger/ShapeEvent.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using EventTypes = VWDAddin.VisioDefinitions.VISIO_EVENT_TYPES;
+
+namespace VWDAddin.VisioLogger
+{
+    public class ShapeEvent : BaseEvent
+    {
+        public ShapeEvent(EventTypes type) : base(type)
+        {
+            // ToDo: add params here
+        }
+
+        #region Members
+        private string m_guid;
+        private string m_mainName;
+        private string m_end1Name;
+        private string m_end1MP;        //Multiplicity
+        private string m_end2Name;
+        private string m_end2MP;
+
+        public string GUID
+        {
+            get { return m_guid; }
+            //set { m_guid = value; }
+        }
+        public string mainName
+        {
+            get { return m_mainName; }
+            //set { m_mainName = value; }
+        }
+        public string end1Name
+        {
+            get { return m_end1Name; }
+            //set { m_end1Name = value; }
+        }
+        public string end1MP
+        {
+            get { return m_end1MP; }
+            //set { m_end1MP = value; }
+        }
+        public string end2Name
+        {
+            get { return m_end2Name; }
+            //set { m_end2Name = value; }
+        }
+        public string end2MP
+        {
+            get { return m_end2MP; }
+            //set { m_end2MP = value; }
+        }
+        #endregion
+    }
+}
