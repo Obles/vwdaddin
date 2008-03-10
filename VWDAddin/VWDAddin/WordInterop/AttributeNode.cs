9550833f1c3068be895201f71fec210c9afccf8b
Nexus.kun <Nexus.kun@4308edd9-fa46-0410-ae85-53b26f51fcf5>
2008-03-10 19:43:06 +0000

    Исправлена ошибка "При сохранении документ в Word'е теряется важная информация"

--- VWDAddin/VWDAddin/WordInterop/AttributeNode.cs
+++ VWDAddin/VWDAddin/WordInterop/AttributeNode.cs
@@ -3,27 +3,32 @@ using System.Collections.Generic;
 using System.Text;
 using System.Xml;
 
-namespace VWDAddin {
-  public class AttributeNode {
-    public AttributeNode(WordDocument doc, XmlNode node) {
-      m_attributeNode = node;
-      m_attrName = m_attributeNode.FirstChild.FirstChild.FirstChild.FirstChild.FirstChild.Value.Substring(Definitions.CLASS_ATTR_NAME_PREFIX.Length);
+namespace VWDAddin
+{
+    public class AttributeNode
+    {
+        public AttributeNode(WordDocument doc, XmlNode node)
+        {
+            AttributeXMLNode = node;
+            if (node.ChildNodes.Count > 0)
+                AttrName = AttributeXMLNode.FirstChild.FirstChild.FirstChild.FirstChild.FirstChild.Value.Substring(Definitions.CLASS_ATTR_NAME_PREFIX.Length);
+        }
+
+        public AttributeNode(WordDocument doc, string attribute)
+        {
+            AttrName = attribute;
+            AttributeXMLNode = WordHelpers.CreateCustomNode(doc, "attr_section");
+            XmlNode attrNameNode = WordHelpers.CreateCustomNode(doc, "attr_name");
+            attrNameNode.AppendChild(WordHelpers.CreateTextChildNode(doc, Definitions.CLASS_ATTR_NAME_PREFIX + attribute));
+            AttributeXMLNode.AppendChild(attrNameNode);
+
+            XmlNode attrDescrNode = WordHelpers.CreateCustomNode(doc, "attr_descr");
+            attrDescrNode.AppendChild(WordHelpers.CreateTextChildNode(doc, Definitions.CLASS_ATTR_NAME_DESCR_PREFIX));
+            AttributeXMLNode.AppendChild(attrDescrNode);
+        }
+
+
+        public XmlNode AttributeXMLNode;
+        public string AttrName;
     }
-
-    public AttributeNode(WordDocument doc, string attribute) {
-      m_attrName = attribute;
-      m_attributeNode = WordHelpers.CreateCustomNode(doc, "attr_section", string.Empty);
-      XmlNode attrNameNode = WordHelpers.CreateCustomNode(doc, "attr_name", string.Empty);
-      attrNameNode.AppendChild(WordHelpers.CreateTextChildNode(doc, Definitions.CLASS_ATTR_NAME_PREFIX + attribute));
-      m_attributeNode.AppendChild(attrNameNode);
-
-      XmlNode attrDescrNode = WordHelpers.CreateCustomNode(doc, "attr_descr", string.Empty);
-      attrDescrNode.AppendChild(WordHelpers.CreateTextChildNode(doc, Definitions.CLASS_ATTR_NAME_DESCR_PREFIX));
-      m_attributeNode.AppendChild(attrDescrNode);
-    }
-     
-
-    public XmlNode m_attributeNode;
-    public string m_attrName;
-  }
 }
