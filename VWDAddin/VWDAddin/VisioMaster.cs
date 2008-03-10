695684ca732d08948cce891d482659233c045f6b
im.andrey <im.andrey@4308edd9-fa46-0410-ae85-53b26f51fcf5>
2008-03-10 12:27:36 +0000

    Создание Visio-элементов программным путем

---
+++ VWDAddin/VWDAddin/VisioMaster.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Office.Interop.Visio;
+using System.Diagnostics;
+
+namespace VWDAddin
+{
+    public class VisioMaster
+    {
+        public static Document GetStencil(Documents documents)
+        {
+            try
+            {
+                return documents[Constants.StencilName];
+            }
+            catch
+            {
+                // The stencil is not in the collection; open it as a docked stencil.
+                return documents.OpenEx(Constants.StencilName, (short)VisOpenSaveArgs.visOpenDocked);
+            }
+        }
+
+        public static Shape Drop(
+            Document document,
+            string masterNameU,
+            double pinX,
+            double pinY)
+        {
+            Shape droppedShape = null;
+            try
+            {
+                // Get a master from the stencil by its universal name.
+                Document stencil = GetStencil(document.Application.Documents);
+                Master master = stencil.Masters.get_ItemU(masterNameU);
+
+                // Drop the master on the page
+                droppedShape = document.Pages[1].Drop(master, pinX, pinY);
+            }
+            catch (Exception err)
+            {
+                System.Diagnostics.Debug.WriteLine(err.Message);
+            }
+
+            return droppedShape;
+        }
+
+        public static Shape DropConnection(
+            Shape source,
+            Shape target,
+            string masterNameU,
+            ClassConnections sourcePoint,
+            ClassConnections targetPoint)
+        {
+            Shape connection = Drop(source.Document, masterNameU, 0, 0);
+
+            String s = Connections.Create(source, sourcePoint);
+            connection.get_Cells("BeginX").Formula = s;
+            connection.get_Cells("BeginY").Formula = s;
+
+            s = Connections.Create(target, targetPoint);
+            connection.get_Cells("EndX").Formula = s;
+            connection.get_Cells("EndY").Formula = s;
+
+            return connection;
+        }
+    }
+}
