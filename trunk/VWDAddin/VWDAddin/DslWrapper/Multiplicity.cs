9d97b1c79c78c558598230626c11f83e869d88aa
im.andrey <im.andrey@4308edd9-fa46-0410-ae85-53b26f51fcf5>
2008-03-18 12:34:22 +0000

    Определение совместимой множественности

--- trunk/VWDAddin/VWDAddin/DslWrapper/Multiplicity.cs
+++ trunk/VWDAddin/VWDAddin/DslWrapper/Multiplicity.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
 
 namespace VWDAddin.DslWrapper
 {
@@ -33,6 +35,107 @@ namespace VWDAddin.DslWrapper
                 case Multiplicity.OneMany: return "1..*";
                 default: throw new NotImplementedException();
             }
+        }
+
+        public static Multiplicity Compatible(String value)
+        {
+            Regex rx = new Regex(@"\d+|\*|\+");
+            MatchCollection matches = rx.Matches(value);
+
+            if (matches.Count == 0)
+            {
+                return Multiplicity.ZeroMany;
+            }
+            else if (matches.Count == 1)
+            {
+                return CompatibleOne(matches[0].Groups[0].Value);
+            }
+            else
+            {
+                String fstStr = matches[0].Groups[0].Value;
+                String sndStr = matches[1].Groups[0].Value;
+                int? fst = parseInt(fstStr);
+                int? snd = parseInt(sndStr);
+
+                // сортируем
+                if (fst == null)
+                {
+                    if (snd != null)
+                    {
+                        Swap(ref fst, ref fstStr, ref snd, ref sndStr);
+                    }
+                    else return CompatibleOne(fstStr);
+                }
+                if(snd != null && fst > snd)
+                {
+                    Swap(ref fst, ref fstStr, ref snd, ref sndStr);
+                }
+
+                // определяем наиболее совместимую множественность
+                if (fst > 0)
+                {
+                    if (fst == snd && snd == 1)
+                    {
+                        return Multiplicity.One;
+                    }
+                    else return Multiplicity.OneMany;
+                }
+                else
+                {
+                    if (snd == 1)
+                    {
+                        return Multiplicity.ZeroOne;
+                    }
+                    else return Multiplicity.ZeroMany;
+                }
+            }
+            return Multiplicity.One;
+        }
+
+        private static Multiplicity CompatibleOne(String countStr)
+        {
+            int? count = parseInt(countStr);
+            if (count == null)
+            {
+                switch (countStr)
+                {
+                    case "*": return Multiplicity.ZeroMany;
+                    case "+": return Multiplicity.OneMany;
+                    default: throw new NotSupportedException();
+                }
+            }
+            else if (count == 1)
+            {
+                return Multiplicity.One;
+            }
+            else
+            {
+                return Multiplicity.OneMany;
+            }
+        }
+
+        private static int? parseInt(String value)
+        {
+            try
+            {
+                return int.Parse(value);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private static void Swap(ref int? fst, ref String fstStr, ref int? snd, ref String sndStr)
+        {
+            String tmpStr = fstStr;
+            int? tmp = fst;
+
+            fstStr = sndStr;
+            fst = snd;
+
+            sndStr = tmpStr;
+            snd = tmp;
         }
     }
 }
