a33ab9d9d5f33076e0517eb17578799de456ff36
im.andrey <im.andrey@4308edd9-fa46-0410-ae85-53b26f51fcf5>
2008-03-25 20:01:20 +0000

    Исправление ошибки удаления ассоциации

--- VWDAddin/VWDAddin/VisioLogger/Actions/Associations/AssociationDeleted.cs
+++ VWDAddin/VWDAddin/VisioLogger/Actions/Associations/AssociationDeleted.cs
@@ -24,7 +24,24 @@ namespace VWDAddin.VisioLogger.Actions.Associations
                 case Constants.Association:
                     {
                         DomainRelationship dr = Dsl.Relationships.Find(Connector.GUID) as DomainRelationship;
-                        XmlClassData xcd = Dsl.XmlSerializationBehavior.GetClassData(dr);
+                        XmlClassData xcd;
+
+                        String srcName = dr.Source.RolePlayer;
+                        if (srcName != null)
+                        {
+                            DomainClass dc = Dsl.Classes[srcName] as DomainClass;
+                            xcd = Dsl.XmlSerializationBehavior.GetClassData(dc);
+                            xcd.ElementData.Remove(xcd.GetRelationshipData(dr));
+                        }
+                        String dstName = dr.Target.RolePlayer;
+                        if (dstName != null)
+                        {
+                            DomainClass dc = Dsl.Classes[dstName] as DomainClass;
+                            xcd = Dsl.XmlSerializationBehavior.GetClassData(dc);
+                            xcd.Xml.RemoveAttribute("SerializeId");
+                        }
+
+                        xcd = Dsl.XmlSerializationBehavior.GetClassData(dr);
                         ConnectionBuilder cb = Dsl.GetConnectionBuilder(dr);
 
                         Dsl.Relationships.Remove(dr);
