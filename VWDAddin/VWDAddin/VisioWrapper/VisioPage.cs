70d3f43836b01bf31ac6897007beb605e35dd567
im.andrey <im.andrey@4308edd9-fa46-0410-ae85-53b26f51fcf5>
2008-03-26 13:07:24 +0000

    Дополнительные классы-обертки

---
+++ VWDAddin/VWDAddin/VisioWrapper/VisioPage.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Office.Interop.Visio;
+using System.Diagnostics;
+
+namespace VWDAddin.VisioWrapper
+{
+    public class VisioPage
+    {
+        private Page Page;
+
+        public VisioPage(Page Page)
+        {
+            this.Page = Page;
+            this.classes = new VisioList<VisioClass>(
+                Page.Shapes, 
+                delegate(Shape shape) 
+                {
+                    return VisioHelpers.GetShapeType(shape) == Constants.Class;
+                }
+            );
+            this.relationships = new VisioList<VisioConnector>(
+                Page.Shapes,
+                delegate(Shape shape)
+                {
+                    String type = VisioHelpers.GetShapeType(shape);
+                    return type == Constants.Association 
+                        || type == Constants.Composition;
+                }
+            );
+            this.inheritances = new VisioList<VisioConnector>(
+                Page.Shapes,
+                delegate(Shape shape)
+                {
+                    return VisioHelpers.GetShapeType(shape) == Constants.Generalization;
+                }
+            );
+        }
+
+        public Shape Find(String Guid)
+        {
+            foreach (Shape shape in Page.Shapes)
+            {
+                VisioShape Shape = new VisioShape(shape);
+                if (Shape.GUID == Guid) return shape;
+            }
+            return null;
+        }
+
+        private VisioList<VisioClass> classes;
+        public VisioList<VisioClass> Classes
+        {
+            get { return classes; }
+        }
+
+        private VisioList<VisioConnector> relationships;
+        public VisioList<VisioConnector> Relationships
+        {
+            get { return relationships; }
+        }
+
+        private VisioList<VisioConnector> inheritances;
+        public VisioList<VisioConnector> Inheritances
+        {
+            get { return inheritances; }
+        }
+    }
+}
