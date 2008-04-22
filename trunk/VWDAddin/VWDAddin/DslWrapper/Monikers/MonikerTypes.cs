7f29efca14c5668248db78cf3d1579db21292730
im.andrey <im.andrey@4308edd9-fa46-0410-ae85-53b26f51fcf5>
2008-04-22 09:42:46 +0000

    Система глобального поиска по моникерам

---
+++ trunk/VWDAddin/VWDAddin/DslWrapper/Monikers/MonikerTypes.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VWDAddin.DslWrapper
+{
+    public enum MonikerTypes
+    {
+        DomainClassMoniker,
+        DomainRelationshipMoniker,
+        DomainPropertyMoniker,
+    }
+}
