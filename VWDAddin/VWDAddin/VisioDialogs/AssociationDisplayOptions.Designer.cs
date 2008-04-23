c362aa96a5a369374812e266a3467b7d304c28af
Nexus.kun <Nexus.kun@4308edd9-fa46-0410-ae85-53b26f51fcf5>
2008-04-23 18:14:03 +0000

    улучшены диалоги

--- VWDAddin/VWDAddin/VisioDialogs/AssociationDisplayOptions.Designer.cs
+++ VWDAddin/VWDAddin/VisioDialogs/AssociationDisplayOptions.Designer.cs
@@ -42,6 +42,8 @@ namespace VWDAddin
             // 
             // DisplayName
             // 
+            this.DisplayName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)));
             this.DisplayName.AutoSize = true;
             this.DisplayName.Location = new System.Drawing.Point(12, 47);
             this.DisplayName.Name = "DisplayName";
@@ -53,7 +55,8 @@ namespace VWDAddin
             // 
             // OKButton
             // 
-            this.OKButton.Location = new System.Drawing.Point(12, 147);
+            this.OKButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.OKButton.Location = new System.Drawing.Point(12, 161);
             this.OKButton.Name = "OKButton";
             this.OKButton.Size = new System.Drawing.Size(75, 23);
             this.OKButton.TabIndex = 6;
@@ -63,8 +66,9 @@ namespace VWDAddin
             // 
             // CancelBtn
             // 
+            this.CancelBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.CancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.CancelBtn.Location = new System.Drawing.Point(209, 147);
+            this.CancelBtn.Location = new System.Drawing.Point(227, 161);
             this.CancelBtn.Name = "CancelBtn";
             this.CancelBtn.Size = new System.Drawing.Size(75, 23);
             this.CancelBtn.TabIndex = 7;
@@ -74,8 +78,10 @@ namespace VWDAddin
             // 
             // DisplayEnd2Name
             // 
+            this.DisplayEnd2Name.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.DisplayEnd2Name.AutoSize = true;
-            this.DisplayEnd2Name.Location = new System.Drawing.Point(160, 82);
+            this.DisplayEnd2Name.Location = new System.Drawing.Point(169, 82);
             this.DisplayEnd2Name.Name = "DisplayEnd2Name";
             this.DisplayEnd2Name.Size = new System.Drawing.Size(116, 17);
             this.DisplayEnd2Name.TabIndex = 3;
@@ -85,6 +91,8 @@ namespace VWDAddin
             // 
             // DisplayEnd1Name
             // 
+            this.DisplayEnd1Name.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)));
             this.DisplayEnd1Name.AutoSize = true;
             this.DisplayEnd1Name.Location = new System.Drawing.Point(12, 82);
             this.DisplayEnd1Name.Name = "DisplayEnd1Name";
@@ -96,6 +104,8 @@ namespace VWDAddin
             // 
             // DisplayEnd1MP
             // 
+            this.DisplayEnd1MP.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)));
             this.DisplayEnd1MP.AutoSize = true;
             this.DisplayEnd1MP.Location = new System.Drawing.Point(12, 117);
             this.DisplayEnd1MP.Name = "DisplayEnd1MP";
@@ -107,8 +117,10 @@ namespace VWDAddin
             // 
             // DisplayEnd2MP
             // 
+            this.DisplayEnd2MP.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.DisplayEnd2MP.AutoSize = true;
-            this.DisplayEnd2MP.Location = new System.Drawing.Point(160, 117);
+            this.DisplayEnd2MP.Location = new System.Drawing.Point(169, 117);
             this.DisplayEnd2MP.Name = "DisplayEnd2MP";
             this.DisplayEnd2MP.Size = new System.Drawing.Size(137, 17);
             this.DisplayEnd2MP.TabIndex = 5;
@@ -118,6 +130,8 @@ namespace VWDAddin
             // 
             // DisplayArrows
             // 
+            this.DisplayArrows.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)));
             this.DisplayArrows.AutoSize = true;
             this.DisplayArrows.Location = new System.Drawing.Point(12, 12);
             this.DisplayArrows.Name = "DisplayArrows";
@@ -133,7 +147,7 @@ namespace VWDAddin
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.CancelBtn;
-            this.ClientSize = new System.Drawing.Size(296, 176);
+            this.ClientSize = new System.Drawing.Size(314, 196);
             this.Controls.Add(this.DisplayArrows);
             this.Controls.Add(this.DisplayEnd2MP);
             this.Controls.Add(this.DisplayEnd1MP);
--- VWDAddin/VWDAddin/VisioDialogs/ClassProperties.Designer.cs
+++ VWDAddin/VWDAddin/VisioDialogs/ClassProperties.Designer.cs
@@ -44,15 +44,19 @@ namespace VWDAddin
             // 
             // ClassNameTextBox
             // 
+            this.ClassNameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.ClassNameTextBox.Location = new System.Drawing.Point(6, 19);
             this.ClassNameTextBox.Name = "ClassNameTextBox";
-            this.ClassNameTextBox.Size = new System.Drawing.Size(200, 20);
+            this.ClassNameTextBox.Size = new System.Drawing.Size(207, 20);
             this.ClassNameTextBox.TabIndex = 0;
             // 
             // CancelBtn
             // 
+            this.CancelBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.CancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.CancelBtn.Location = new System.Drawing.Point(149, 207);
+            this.CancelBtn.Location = new System.Drawing.Point(156, 240);
             this.CancelBtn.Name = "CancelBtn";
             this.CancelBtn.Size = new System.Drawing.Size(75, 23);
             this.CancelBtn.TabIndex = 3;
@@ -62,7 +66,8 @@ namespace VWDAddin
             // 
             // OKButton
             // 
-            this.OKButton.Location = new System.Drawing.Point(12, 207);
+            this.OKButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.OKButton.Location = new System.Drawing.Point(12, 239);
             this.OKButton.Name = "OKButton";
             this.OKButton.Size = new System.Drawing.Size(75, 23);
             this.OKButton.TabIndex = 2;
@@ -72,17 +77,21 @@ namespace VWDAddin
             // 
             // AttrListBox
             // 
+            this.AttrListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.AttrListBox.FormattingEnabled = true;
             this.AttrListBox.HorizontalScrollbar = true;
             this.AttrListBox.Location = new System.Drawing.Point(6, 19);
             this.AttrListBox.Name = "AttrListBox";
-            this.AttrListBox.Size = new System.Drawing.Size(120, 82);
+            this.AttrListBox.Size = new System.Drawing.Size(127, 108);
             this.AttrListBox.TabIndex = 1;
             this.AttrListBox.DoubleClick += new System.EventHandler(this.AtrrListBox_DoubleClick);
             // 
             // AddAttrBtn
             // 
-            this.AddAttrBtn.Location = new System.Drawing.Point(131, 19);
+            this.AddAttrBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.AddAttrBtn.Location = new System.Drawing.Point(138, 19);
             this.AddAttrBtn.Name = "AddAttrBtn";
             this.AddAttrBtn.Size = new System.Drawing.Size(75, 23);
             this.AddAttrBtn.TabIndex = 6;
@@ -92,7 +101,8 @@ namespace VWDAddin
             // 
             // RemoveAttrBtn
             // 
-            this.RemoveAttrBtn.Location = new System.Drawing.Point(131, 48);
+            this.RemoveAttrBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.RemoveAttrBtn.Location = new System.Drawing.Point(138, 48);
             this.RemoveAttrBtn.Name = "RemoveAttrBtn";
             this.RemoveAttrBtn.Size = new System.Drawing.Size(75, 23);
             this.RemoveAttrBtn.TabIndex = 7;
@@ -102,22 +112,27 @@ namespace VWDAddin
             // 
             // AttributesGroupBox
             // 
+            this.AttributesGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.AttributesGroupBox.Controls.Add(this.AttrListBox);
             this.AttributesGroupBox.Controls.Add(this.RemoveAttrBtn);
             this.AttributesGroupBox.Controls.Add(this.AddAttrBtn);
             this.AttributesGroupBox.Location = new System.Drawing.Point(12, 80);
             this.AttributesGroupBox.Name = "AttributesGroupBox";
-            this.AttributesGroupBox.Size = new System.Drawing.Size(212, 109);
+            this.AttributesGroupBox.Size = new System.Drawing.Size(219, 141);
             this.AttributesGroupBox.TabIndex = 8;
             this.AttributesGroupBox.TabStop = false;
             this.AttributesGroupBox.Text = "Attributes";
             // 
             // ClassNameGroupBox
             // 
+            this.ClassNameGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.ClassNameGroupBox.Controls.Add(this.ClassNameTextBox);
             this.ClassNameGroupBox.Location = new System.Drawing.Point(12, 12);
             this.ClassNameGroupBox.Name = "ClassNameGroupBox";
-            this.ClassNameGroupBox.Size = new System.Drawing.Size(212, 53);
+            this.ClassNameGroupBox.Size = new System.Drawing.Size(219, 55);
             this.ClassNameGroupBox.TabIndex = 9;
             this.ClassNameGroupBox.TabStop = false;
             this.ClassNameGroupBox.Text = "Class name";
@@ -128,7 +143,7 @@ namespace VWDAddin
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.CancelBtn;
-            this.ClientSize = new System.Drawing.Size(236, 242);
+            this.ClientSize = new System.Drawing.Size(243, 275);
             this.Controls.Add(this.ClassNameGroupBox);
             this.Controls.Add(this.AttributesGroupBox);
             this.Controls.Add(this.OKButton);
