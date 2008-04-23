af09efed9cfec774ca50a6d399e2c302a89e8da5
Nexus.kun <Nexus.kun@4308edd9-fa46-0410-ae85-53b26f51fcf5>
2008-04-23 18:14:03 +0000

    улучшены диалоги

--- trunk/VWDAddin/VWDAddin/VisioDialogs/ChangeValue.Designer.cs
+++ trunk/VWDAddin/VWDAddin/VisioDialogs/ChangeValue.Designer.cs
@@ -30,14 +30,16 @@ namespace VWDAddin
         {
             this.OKBtn = new System.Windows.Forms.Button();
             this.CancelBtn = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.ValueTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // OKBtn
             // 
+            this.OKBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.OKBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.OKBtn.Location = new System.Drawing.Point(12, 63);
+            this.OKBtn.Location = new System.Drawing.Point(12, 88);
             this.OKBtn.Name = "OKBtn";
             this.OKBtn.Size = new System.Drawing.Size(75, 23);
             this.OKBtn.TabIndex = 1;
@@ -47,8 +49,9 @@ namespace VWDAddin
             // 
             // CancelBtn
             // 
+            this.CancelBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.CancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.CancelBtn.Location = new System.Drawing.Point(142, 63);
+            this.CancelBtn.Location = new System.Drawing.Point(129, 88);
             this.CancelBtn.Name = "CancelBtn";
             this.CancelBtn.Size = new System.Drawing.Size(75, 23);
             this.CancelBtn.TabIndex = 2;
@@ -56,21 +59,26 @@ namespace VWDAddin
             this.CancelBtn.UseVisualStyleBackColor = true;
             this.CancelBtn.Click += new System.EventHandler(this.CancelBtn_Click);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(171, 15);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Attribute";
-            // 
             // ValueTextBox
             // 
-            this.ValueTextBox.Location = new System.Drawing.Point(12, 12);
+            this.ValueTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.ValueTextBox.Location = new System.Drawing.Point(6, 19);
             this.ValueTextBox.Name = "ValueTextBox";
-            this.ValueTextBox.Size = new System.Drawing.Size(131, 20);
+            this.ValueTextBox.Size = new System.Drawing.Size(180, 20);
             this.ValueTextBox.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.ValueTextBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(192, 47);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Attribute";
             // 
             // ChangeValue
             // 
@@ -78,16 +86,16 @@ namespace VWDAddin
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.CancelBtn;
-            this.ClientSize = new System.Drawing.Size(227, 96);
-            this.Controls.Add(this.ValueTextBox);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(216, 123);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.CancelBtn);
             this.Controls.Add(this.OKBtn);
             this.Name = "ChangeValue";
             this.Text = "Change attribute";
             this.TopMost = true;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -95,7 +103,7 @@ namespace VWDAddin
 
         private System.Windows.Forms.Button OKBtn;
         private System.Windows.Forms.Button CancelBtn;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox ValueTextBox;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
